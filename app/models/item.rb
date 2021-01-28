d52a63c8253deacc9585aecf900f3db252fefe89
kurage0516 <73592225+kurage0516@users.noreply.github.com>
2021-01-28 23:27:16 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,20 @@ class Item < ApplicationRecord
 
   belongs_to       :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image,            unless: :was_attached?
+    validates :text
+    validates :category_id
+    validates :product_state_id
+    validates :delivery_fee_id
+    validates :area_id
+    validates :day_id
+    validates :price,            format: { with: /\A[０-９]+\z/}, numericality: { greater_than_or_equal_to: 300 }, numericality: { less_than_or_equal_to: 9999999 }
+  end
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
