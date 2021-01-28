e24291465122c7abe027baea1060d8fdc6ec5605
kurage0516 <73592225+kurage0516@users.noreply.github.com>
2021-01-28 23:00:37 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :day
+  belongs_to :delivery_fee
+  belongs_to :product_state
+
   belongs_to       :user
   has_one_attached :image
 end
