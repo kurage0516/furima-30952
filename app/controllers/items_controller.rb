ce46a7184e1f7b6267ab102c8705de855d7da180
kurage0516 <73592225+kurage0516@users.noreply.github.com>
2021-02-01 22:16:46 +0900

    商品出品ページの表示と保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
 
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(@item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:content, :image, :category_id, :product_state_id, :delivery_fee_id, :area_id, :day_id).merge(user_id: current_user.id)
   end
 
 end
